cd99a384278f77fd53656b5d18d4017640ad47de
atousley <annette.tousley@gmail.com>
2016-07-22 21:50:45 -0500

    added new user controler and routing

---
+++ public/assets/scripts/controllers/newusercontroller.js
@@ -0,0 +1,6 @@
+myApp.controller('NewUserController', ['$scope', 'DataFactory', '$http', function($scope, DataFactory, $http) {
+
+//this might not end up being needed it the future if new users don't need additional action
+
+
+}]);
