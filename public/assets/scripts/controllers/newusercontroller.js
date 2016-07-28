6702dcf327ab9cd7c8e69280857c3fd8539e6523
atousley <annette.tousley@gmail.com>
2016-07-27 22:26:49 -0500

    Added text contact link and button to new user welcome page

--- public/assets/scripts/controllers/newusercontroller.js
+++ public/assets/scripts/controllers/newusercontroller.js
@@ -1,6 +1,15 @@
-myApp.controller('NewUserController', ['$scope', 'DataFactory', '$http', function($scope, DataFactory, $http) {
+myApp.controller('NewUserController', ['$scope', '$location', function($scope, $location) {
 
 //this might not end up being needed it the future if new users don't need additional action
+
+
+    $scope.reroute = function() {
+
+        console.log('i work ok?');
+
+        $location.path('/board');
+
+    }
 
 
 }]);
